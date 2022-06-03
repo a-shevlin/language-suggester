a696e678606bcf2db93e71f1e1db77b61b837bfb
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-03 12:08:16 -0700

    BUGFIX scripts.js line 6 from # to :.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,11 @@ $(document).ready(function() {
     event.preventDefault();
     const name = $("input#name").val();
     const age = parseInt($("#age").val());
-    const color = parseInt($("input#radio[name=color]:checked").val());
+    const color = parseInt($("input:radio[name=color]:checked").val());
     const place = parseInt($("#place").val());
     const toDo = parseInt($("#toDo").val());
     const all = age + color + place + toDo;
+    console.log(all);
 
     if (all >= 1 && all < 100) {
       $("#html").show();
